5765b6b74c71cbd70d5c0250a58fe72df1d267d5
mauriciopintos <mauricioenrique.pintos@estudiantes.unahur.edu.ar>
2023-05-11 17:28:06 -0300

    Subo los avances de los scripts

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+// var mensaje = 'Hola mundo'
+// console.log(mensaje);
+// alert(mensaje);
+
+//Mostrar el formulario
+function mostrarFormulario(id, url) {
+    let formularioFlotante = document.getElementById(id);
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+      if (req.readyState === 4 && req.status === 200) {
+        formularioFlotante.innerHTML = req.responseText;
+        formularioFlotante.style.display = "block";
+      }
+    };
+    req.open("GET", url, true);
+    req.send();
+}
+
+// // Ocultar el formulario
+function ocultarFormulario(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+// document.getElementById("formulario-flotante").addEventListener("click", function(event) {
+//     if (event.target === this) {
+//       this.style.display = "none";
+//     }
+//   });
+
+
+// document.getElementById("cerrar").addEventListener("click", ocultarFlotante);
+function cargarPais(){
+  // TODO
+}
+
+function agregarDato(){
+  // TODO
+}
